3c492fc44c137fead62aadbaa846b0dda7dd1910
kiosnetdasarata <itkiosnet.dasarata@gmail.com>
2024-01-25 17:54:42 +0700

    ad

--- app.js
+++ app.js
@@ -26,7 +26,29 @@ const db = mysql.createConnection({
 
 app.use(express.json());
 
-app.post('/api/store-customer', createCustomer);
+// app.post('/api/store-customer', createCustomer);
+
+// Endpoint untuk menyimpan data ke MySQL
+app.post('/api/store-customer', (req, res) => {
+  // Ambil data dari permintaan
+  const data = req.body;
+
+  // Query untuk menyimpan data ke tabel tertentu
+  const query = 'INSERT INTO message_customer (id_customer, nama, paket, harga, aktif, expired, no_tlpn, message_h_min_2) VALUES (?, ?)';
+  const values = [data.data1, data.data2, data.data3, data.data4, data.data5, data.data6, data.data7, data.data8];
+
+  // Jalankan query
+  db.query(query, values, (err, result) => {
+    if (err) {
+      console.error('Error saat menyimpan data ke MySQL:', err);
+      res.status(500).json({ error: 'Gagal menyimpan data ke MySQL' });
+      return;
+    }
+
+    console.log('Data berhasil disimpan ke MySQL');
+    res.json({ status: 'Data berhasil disimpan ke MySQL' });
+  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
