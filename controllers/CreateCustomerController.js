ba7a8337ea57ed265935638ab6a7461a007e032c
kiosnetdasarata <itkiosnet.dasarata@gmail.com>
2024-01-25 18:00:18 +0700

    x

--- controllers/CreateCustomerController.js
+++ controllers/CreateCustomerController.js
@@ -1,18 +1,18 @@
 import { insert_customer } from "../query/reminder_query.js";
 import { customer_h_min_two } from "../database/db.js";
 
-export async function createCustomer(req, res){
+function createCustomer(req, res){
     const data = req.body;
     const values = [data.data1, data.data2, data.data3, data.data4, data.data5, data.data6, data.data7];
 
     customer_h_min_two.query(insert_customer, values, (err, result) => {
         if (err) {
-        //   console.error('Error saat menyimpan data ke MySQL:', err);
+          console.error('Error saat menyimpan data ke MySQL:', err);
           res.status(500).json({ error: 'Gagal menyimpan data ke MySQL' });
           return;
         }
     
-        // console.log('Data berhasil disimpan ke MySQL');
+        console.log('Data berhasil disimpan ke MySQL');
         res.json({ status: 'Data berhasil disimpan ke MySQL' });
       });
 }
