25c9383bc3a0af7e970439da82c0118dc78208f7
kiosnetdasarata <itkiosnet.dasarata@gmail.com>
2024-01-25 17:43:20 +0700

    kebanyakan

--- query/reminder_query.js
+++ query/reminder_query.js
@@ -1,5 +1,5 @@
 export const query_min_two = 'SELECT * FROM reminder_min_two WHERE message = 0';
 
-export const insert_customer = 'INSERT INTO reminder_min_two (id_customer, nama, paket, harga, aktif, expired, no_tlpn) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+export const insert_customer = 'INSERT INTO reminder_min_two (id_customer, nama, paket, harga, aktif, expired, no_tlpn) VALUES (?, ?, ?, ?, ?, ?, ?)';
 
 export const update_customer = 'UPDATE reminder_min_two SET message = 1 WHERE id = ?';
