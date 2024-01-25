9c505bceb22fac7323f495603a5fb4ed659a4c32
kiosnetdasarata <itkiosnet.dasarata@gmail.com>
2024-01-25 18:01:28 +0700

    asdf

--- controllers/CreateCustomerController.js
+++ controllers/CreateCustomerController.js
@@ -1,7 +1,7 @@
 import { insert_customer } from "../query/reminder_query.js";
 import { customer_h_min_two } from "../database/db.js";
 
-function createCustomer(req, res){
+export function createCustomer(req, res){
     const data = req.body;
     const values = [data.data1, data.data2, data.data3, data.data4, data.data5, data.data6, data.data7];
 
