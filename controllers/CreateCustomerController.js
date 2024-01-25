b4e5667c39b7fe8ae4ff40ae362e92fdf20d8573
kiosnetdasarata <itkiosnet.dasarata@gmail.com>
2024-01-25 17:04:10 +0700

    da

--- controllers/CreateCustomerController.js
+++ controllers/CreateCustomerController.js
@@ -1,10 +1,11 @@
 import { insert_customer } from "../query/reminder_query.js";
+import { customer_h_min_two } from "../database/db.js";
 
 export async function createCustomer(data){
     try{
         const data = req.body;
         const values = [data.data1, data.data2, data.data3, data.data4, data.data5, data.data6, data.data7];
-        await insert_customer.query(values)
+        await customer_h_min_two.query(insert_customer, values)
         return {message:"succsss"}
     }catch(error){
         return {message:"error",code_error: error.errno}
