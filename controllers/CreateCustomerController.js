b5f35d275073b214db61d7bc2a9d1cbf13dda441
kiosnetdasarata <itkiosnet.dasarata@gmail.com>
2024-01-25 17:51:29 +0700

    sd

--- controllers/CreateCustomerController.js
+++ controllers/CreateCustomerController.js
@@ -14,29 +14,42 @@ export async function createCustomer(req, res){
     const data = req.body;
     const values = [data.data1, data.data2, data.data3, data.data4, data.data5, data.data6, data.data7];
 
-    const dd = await insertData(values);
+    const dd = await insertData(values, res);
     console.log(dd.message);
     return dd;
 
 }
 
-async function insertData(data)
-{
-    try{
-        await customer_h_min_two.query(insert_customer, data);
-    //     await customer_h_min_two.query(insert_customer, data, (err, result) => {
-    //     if (err) {
-    //         console.error('Error saat menyimpan data ke MySQL:', err);
-    //         res.status(500).json({ error: 'Gagal menyimpan data ke MySQL' });
-    //         return;
-    //     }
+function insertData(values, res) {
+    customer_h_min_two.query(insert_customer, values, (err, result) => {
+        if (err) {
+            console.error('Error saat menyimpan data ke MySQL:', err);
+            res.status(500).json({ error: 'Gagal menyimpan data ke MySQL' });
+            return;
+        }
 
-    //     console.log('Data berhasil disimpan ke MySQL');
-    //     res.json({ status: 'Data berhasil disimpan ke MySQL' });
-    // });
-        return {message:"success"}
-    }catch(error){
-        console.log(error);
-        return {message:"error",code_error: error.errno}
-    }
+        console.log('Data berhasil disimpan ke MySQL');
+        res.json({ status: 'Data berhasil disimpan ke MySQL' });
+    });
 }
+
+// async function insertData(data)
+// {
+//     try{
+//         await customer_h_min_two.query(insert_customer, data);
+//     //     await customer_h_min_two.query(insert_customer, data, (err, result) => {
+//     //     if (err) {
+//     //         console.error('Error saat menyimpan data ke MySQL:', err);
+//     //         res.status(500).json({ error: 'Gagal menyimpan data ke MySQL' });
+//     //         return;
+//     //     }
+
+//     //     console.log('Data berhasil disimpan ke MySQL');
+//     //     res.json({ status: 'Data berhasil disimpan ke MySQL' });
+//     // });
+//         return {message:"success"}
+//     }catch(error){
+//         console.log(error);
+//         return {message:"error",code_error: error.errno}
+//     }
+// }
